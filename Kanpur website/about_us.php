340bdbb5dcd932f75fb5971add863c70446ffa68
shivanshu tiwari <45694330+tiwarishivanshu@users.noreply.github.com>
2021-03-30 23:28:11 +0530

    Add files via upload

---
+++ Kanpur website/about_us.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/about_usstyle.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Kanpuriya Developer</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+       <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="visitplaces.php">Have u visited?</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+       <li class="nav-item active">
+        <a class="nav-link" href="about_us.php">About<span class="sr-only">(current)</span></a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li> 
+      
+     
+    </ul>
+  </div>
+</nav>
+
+
+</div>
+    <div class="container-fluid" style="margin-top:5%;max-width: 50%;min-width: 85%" >
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-12">
+            <div class="container">
+                <h2 style="text-align: center;" >This Is Who We Are<br><strong><a href="#" data-toggle="tooltip" data-placement="top" title="Still Learning!" style="text-decoration: none; color:black;">Student's</a></strong> making efforts to stand out</h2>
+
+             </div>
+
+            <div class="container">
+              
+                  <p style="text-align: center;">
+                      <img src="images/about_us/grouppic.jpg" class="img-fluid" width="100%" height="100%" style="border: 1px solid black; padding: 2px;max-height: 550px; min-height: 220px;max-width: 500px;min-width: 200px;">
+                  </p>
+               
+            </div>
+          </div>
+
+      <div class="col-lg-6 col-md-6 col-12 mt-2">
+          <div class="container" style="margin-top:23%;">
+
+              <div class="card-deck">
+
+
+                  <div class="card bcka" style="width:70%; margin: 0 auto; float: none; margin-bottom: 10px;">
+                          <p style="text-align: center;">
+                          <img class="card-img-top" src="images/about_us/rawat.png" alt="Card image" style="width:50%;margin-top:2%;"></p>
+
+                          <div class="card-body">
+                            <h4 class="card-title" style="text-align: center;">Shubham Rawat</h4>
+                            <p class="card-text" style="text-align: center;">Marketing</p>
+                            
+                          </div>
+                  </div>
+
+
+
+                  <div class="card bckb" style="width:70%;margin: 0 auto; float: none; margin-bottom: 10px;">
+                          <p style="text-align: center;">
+                          <img class="card-img-top" src="images/about_us/tanmay.png" alt="Card image" style="width:50%;margin-top:2%;"></p>
+                            <div class="card-body">
+                              <h4 class="card-title" style="text-align: center;">Vedansh Dixit</h4>
+                              <p class="card-text" style="text-align: center;">Developer</p>
+                            </div>
+                  </div>
+
+            </div>
+          </div>
+              
+          <div class="container" style="margin-top:2%;">
+              <div class="card-deck">
+
+
+                  <div class="card bckc" style="width:70%;margin: 0 auto; float: none; margin-bottom: 10px;">
+                      <p style="text-align: center;">
+                      <img class="card-img-top" src="images/about_us/shivam.png" alt="Card image" style="width:50%;margin-top:2%;"></p>
+                          <div class="card-body">
+                            <h4 class="card-title" style="text-align: center;">Shivam Pal</h4>
+                            <p class="card-text" style="text-align: center;">Developer</p>
+                          </div>
+                  </div>
+                  
+
+                  <div class="card bckd" style="width:70%;margin: 0 auto; float: none; margin-bottom: 10px;">
+                    <p style="text-align: center;">
+                    <img class="card-img-top" src="images/about_us/shivanshu.png" alt="Card image" style="width:50%;margin-top:2%;"></p>
+                          <div class="card-body">
+                            <h4 class="card-title" style="text-align: center;">Shivanshu Tiwari</h4>
+                            <p class="card-text" style="text-align: center;">Developer</p>
+                          </div>
+                  </div>
+
+
+                  
+              </div>
+          </div>
+          
+      </div>
+      
+    </div>
+  </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ Kanpur website/index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>kanpur home page</title>
+
+
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Kanpuriya Developer</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="visitplaces.php">Have u visited?</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="about_us.php">About Us</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+      
+     
+    </ul>
+    <!-- <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form> -->
+  </div>
+</nav>
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="3"></li>
+    <li data-target="#demo" data-slide-to="4"></li>
+    <li data-target="#demo" data-slide-to="5"></li>
+    <li data-target="#demo" data-slide-to="6"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/winter1.jpg" alt="Winter jackets in kanpur" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Winter jackets in kanpur</h3>
+        <p>We had such a great time in Kanpur!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/winter.jpg" alt="Winter jackets in kanpur1" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Winter jackets in kanpur</h3>
+        <p>Thank you, Kanpur!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/rainy rikshaw.jpg" alt="Kanpur Street" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Kanpur Street</h3>
+        <p>We love the Desi!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/kanpur central.jpg" alt="Kanpur Central" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Kanpur Central</h3>
+        <p>We love the Desi!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/local_market.jpg" alt="local_market" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>local_market</h3>
+        <p>We love Desi!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/chaurha.jpg" alt="" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>chaurha</h3>chaurha
+        <p>We love Desi!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/bairaj.jpg" alt="bairaj" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Ganga Bairaj</h3>
+        <p>We love Desi!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">About Us</h2>
+	</div>
+        <div class="container-fluid">
+          <div class="row">
+          <div class="col-lg-6 col-md-6 col-12">
+            <img src="images/pankimandir.jpg" class="img-fluid">
+          </div>
+          <div class="col-lg-6 col-md-6 col-12">
+          <h2>Panki Temple</h2>
+          <p class="py-2">
+            Panki is a suburb of metropolitan Kanpur, India, situated about 20 km from Kanpur Central on the NH 2 to Delhi. The region is famous for Panki Temple and Panki Thermal Power Station which is among the few thermal energy units in Northern India. Panki is an industrial and resedential region and has recently seen large economic developments.The site is also famous for being the Logistics Hub in Kanpur region. Panki Logistics Park is among the largest logistics park of Northern India.
+          </p>
+          <a href="about.php" class="btn btn-success">Check More</a>
+          </div>
+        </div>
+        </div>
+        <div class="container-fluid py-5">
+          <div class="row">
+          
+          <div class="col-lg-6 col-md-6 col-12">
+          <h2>Blue World Theme Park</h2>
+          <p class="py-2">
+           Blue World Theme Park is Kanpur’s first mega themed entertainment destination promising visitors from around the world the excitement of epic adventure zones in one location. 25 acres of land featuring over 25 rides and 10 gigantic slides including Dry Rides, aquatic wonderland, 7D Theater, Sky dive and India’s biggest private musical fountain and laser show.
+          </p>
+          <a href="about.php" class="btn btn-success">Check More</a>
+          </div>
+          <div class="col-lg-6 col-md-6 col-12">
+            <img src="images/blueworld.png" class="img-fluid">
+          </div>
+        </div>
+        </div>
+        <div class="container-fluid">
+          <div class="row">
+          <div class="col-lg-6 col-md-6 col-12">
+            <img src="images/New folderjk templeimg/jk2.jpg" class="img-fluid jkimg">
+          </div>
+          <div class="col-lg-6 col-md-6 col-12">
+          <h2>JK(Radha Krishna) Temple</h2>
+          <p class="py-2">
+            JK Temple (Juggilal Kamlapat temple) is a Hindu temple in the Indian city of Kanpur, Uttar Pradesh.It is considered to be a unique blend of ancient and modern architecture. The mandapa of the temple has been constructed with high roof for adequate ventilation of light and air. The temple was constructed by JK Trust and major maintenance expenses of the temple also comes from the trust fund. The temple is also popularly known as Radhakrishna Temple.
+          </p>
+          <a href="about.php" class="btn btn-success">Check More</a>
+          </div>
+        </div>
+        </div>
+        <div class="container-fluid py-5">
+          <div class="row">
+          <div class="col-lg-6 col-md-6 col-12">
+          <h2> Sudhanshu Ji Maharaj Ashram</h2>
+          <p class="py-2">
+           Sudhanshu Ji Maharaj Ashram is the most famous people attraction in Kanpur, here many god temple: Lord Krishna, Lord Shiva, Goddess Radha, Goddess Parvati, and Hanuman located inside the ashram. In this temple a beautiful garden located inside the ashram. The Sudhanshu Ji Maharaj is located which gives peace to the mind and soul. For poor people, there are non-profit homes which are built behind the temple. Sudhanshu Ji Maharaj Ashram is the most famous attraction of people in Kanpur, here many god temples. Lord Krishna, Lord Shiva, Goddess Radha, Goddess Parvati, and Hanuman all located inside the ashram. 
+          </p>
+          <a href="about.php" class="btn btn-success">Check More</a>
+          </div>
+          <div class="col-lg-6 col-md-6 col-12">
+            <img src="images/shudhanshuji.jpg" class="img-fluid">
+          </div>
+        </div>
+        </div>
+        <h3 class="text-center py-2">Ka babua! Kanpur aaye ho ka kbhi :)</h3>
+</section>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
